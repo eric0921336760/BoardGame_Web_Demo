fa874a5380dd18a5d46141c3c6cab56e92eb49bd
Alex Hsu <eric0921336760@gmail.com>
2018-01-18 23:43:29 +0800

    新增影片暫停功能與個人簡介

--- js/test.js
+++ js/test.js
@@ -6,6 +6,29 @@ $(document).ready(function(){
         height:400
     });
 
+       $("ul li a").on("click",function(){
+                var a_elm =$(this);
+                var target_id = a_elm.attr("href");
+
+                var topTo = 0;
+
+                if(!target_id!="#"){
+                    topTo = $(target_id).position().top;
+                }
+
+                $("html,body").animate({
+                    scrollTop:topTo
+                },600,function(){
+                    $("ul li[class='active']").removeClass('active');
+
+                    a_elm.parent().addClass('class');
+                });
+
+                return false;
+            });
+    
+
+
 });
 
 $("a.change_volume").on("click",function(){
@@ -25,3 +48,20 @@ $("a.change_volume").on("click",function(){
 
 
 });
+
+$("div.myvideo").on("click",function(){
+    console.log("haha");
+    var video=$("video")[0];
+    
+    if(video.paused){
+        video.play();
+    }else{
+        
+        video.pause();
+    }
+        
+     
+    
+});
+
+
